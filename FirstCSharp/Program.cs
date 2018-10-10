a8e478fe40542476d6dd806a95c8b4d2160136a8
Insiya Hunaid <insiya.hakim5152@gmail.com>
2018-10-09 19:28:46 -0500

    first

---
+++ FirstCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FirstCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool crazy = true;
+            if (!crazy)
+            {
+                Console.WriteLine("Let's party!");
+            }
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SecondCSharp/SecondCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SecondCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            bool crazy = true;
+            if (!crazy)
+            {
+                Console.WriteLine("Let's party!");
+            }
+        }
+    }
+}
